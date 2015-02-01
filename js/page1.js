1c401fb9ac0d62c5bfe8cf9bcced856f801eaa35
ai4281 <ai4281@gmail.com>
2015-02-01 14:37:27 -0800

    page 1 updated

--- js/page1.js
+++ js/page1.js
@@ -5,15 +5,20 @@
 var PageOneContent = React.createClass({
   displayName: 'PageOneContent',
 
+ //  addScore: function() {
+ //   score++;
+ //   console.log(score);
+ // },
+
   render: function() {
     return React.DOM.div(null,
-      React.DOM.h2(null, 'Example Question'),
-      React.DOM.p(null, 'This is a sample question'),
-      React.DOM.p(null, 'There will be 4 answers, but only 1 will be correct'),
-      React.DOM.p(null, 'Answer 1'),
-      React.DOM.p(null, 'Answer 2'),
-      React.DOM.p(null, 'Answer 3'),
-      React.DOM.p(null, 'Answer 4'),
+      React.DOM.h2(null, 'Question 1'),
+      React.DOM.p(null, 'Which food item belongs in an earthquake emergency supplies package?'),
+      React.DOM.p(null, 'Pick the best one.'),
+      React.DOM.p(null, React.DOM.button(null, 'Meat Loaf')),
+      React.DOM.p(null, React.DOM.button(null, 'Watermelon')),
+      React.DOM.p(null, React.DOM.button(null, 'Raw Eggs')),
+      React.DOM.p({onClick: addScore}, React.DOM.button(null, 'Nuts and Trail Mixes')),
       React.DOM.h3(null, ''),
       JQueryMobileButton({href:'#two'}, 'Next Question')
     );
