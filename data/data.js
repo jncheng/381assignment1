9a34e9a7cdb29cb0fbab8f84d3d2c2b8deccd912
ai4281 <ai4281@gmail.com>
2015-02-07 13:32:03 -0800

    change data

--- data/data.js
+++ data/data.js
@@ -35,7 +35,7 @@ var pageVisitedBool= [
 	//4
 	false,
 	//5
-	false,
+	false
 
 ];
 
@@ -47,46 +47,4 @@ var pageHash = [
 	'#four',
 	'#five'
 ]
-=======
-var data = [{
-	question: 'Which belongs in an earthquake',
-	choices: [ 
-	{answer: 'Meet loaf', correct: false},
-	{answer: 'Watermelon', correct: false},
-	{answer: 'Trail Mix', correct: true},
-	{answer: 'Pineapple', correct: false}
-	]
-},{
-	question: 'How should you behave during an earthquake?',
-	choices:[
-	{answer: 'Panic and run', correct: false},
-	{answer: 'Go on Facebook', correct: false},
-	{answer: 'Keep calm', correct: true},
-	{answer: 'Dance', correct: false}
-	]
-},{
-	question: 'Which belongs in an earthquake',
-	choices:[
-	{answer: 'Meet loaf', correct: false},
-	{answer: 'Watermelon', correct: false},
-	{answer: 'Trail Mix', correct: true},
-	{answer: 'Pineapple', correct: false}
-	]
-},{
-	question: 'Which belongs in an earthquake',
-	choices:[
-	{answer: 'Meet loaf', correct: false},
-	{answer: 'Watermelon', correct: false},
-	{answer: 'Trail Mix', correct: true},
-	{answer: 'Pineapple', correct: false}
-	]
-},{
-	question: 'Which belongs in an earthquake',
-	choices:[
-	{answer: 'Meet loaf', correct: false},
-	{answer: 'Watermelon', correct: false},
-	{answer: 'Trail Mix', correct: true},
-	{answer: 'Pineapple', correct: false}
-	]
-}];
->>>>>>> FETCH_HEAD
+
