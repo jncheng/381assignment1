7ec815b98ba419ee1c32697f618dbf53dc743c96
jncheng <jncheng@sfu.ca>
2015-02-07 12:57:28 -0800

    centered questions

--- data/data.js
+++ data/data.js
@@ -1,4 +1,41 @@
-var data = [
-	{displayName: 'PageOneContent'},
-	{question: 'Which belongs in an earthquake emergency supplies package?'}
-];
+var data = [{
+	question: 'Which belongs in an earthquake',
+	choices: [ 
+	{answer: 'Meet loaf', correct: false},
+	{answer: 'Watermelon', correct: false},
+	{answer: 'Trail Mix', correct: true},
+	{answer: 'Pineapple', correct: false}
+	]
+},{
+	question: 'How should you behave during an earthquake?',
+	choices:[
+	{answer: 'Panic and run', correct: false},
+	{answer: 'Go on Facebook', correct: false},
+	{answer: 'Keep calm', correct: true},
+	{answer: 'Dance', correct: false}
+	]
+},{
+	question: 'Which belongs in an earthquake',
+	choices:[
+	{answer: 'Meet loaf', correct: false},
+	{answer: 'Watermelon', correct: false},
+	{answer: 'Trail Mix', correct: true},
+	{answer: 'Pineapple', correct: false}
+	]
+},{
+	question: 'Which belongs in an earthquake',
+	choices:[
+	{answer: 'Meet loaf', correct: false},
+	{answer: 'Watermelon', correct: false},
+	{answer: 'Trail Mix', correct: true},
+	{answer: 'Pineapple', correct: false}
+	]
+},{
+	question: 'Which belongs in an earthquake',
+	choices:[
+	{answer: 'Meet loaf', correct: false},
+	{answer: 'Watermelon', correct: false},
+	{answer: 'Trail Mix', correct: true},
+	{answer: 'Pineapple', correct: false}
+	]
+}];
